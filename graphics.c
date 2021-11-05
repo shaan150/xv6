1ad1287310935f94f7b1a9d97fc62675fced5f61
wrippin <w.rippin@derby.ac.uk>
2021-11-05 10:28:05 +0000

    Initial respository setup. Based on xv6-udo.

---
+++ graphics.c
@@ -0,0 +1,11 @@
+#include "types.h"
+#include "defs.h"
+
+void clear320x200x256() {
+	// You need to put code to clear the video buffer here.  Initially, 
+	// you might just set each pixel to black in a nested loop, but think
+	// about faster ways to do it. 
+	//
+	// This function is called from videosetmode.
+}
+
